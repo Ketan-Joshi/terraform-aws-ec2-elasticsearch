b37369332e64efe592ed989a53f714ac72a06b29
Ketan Joshi <ketanjoshi@TTNs-MacBook-Pro.local>
2023-03-03 16:49:05 +0530

    Fixes

--- examples/main.tf
+++ examples/main.tf
@@ -1,7 +1,7 @@
 module "prometheus" {
     source = "../"
-    instance_type_elasticsearch = "t3.medium"
-    volume_size_elasticsearch = "30"
+    instance_type_elasticsearch = "t3.large"
+    volume_size_elasticsearch = "50"
     pem_key_name = "elasticsearch"
     vpc_cidr_block = ""
     vpc_id = ""
