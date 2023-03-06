c8f26c6353c4470e411396eda945e423c1fa821f
Ketan Joshi <ketanjoshi@TTNs-MacBook-Pro.local>
2023-03-06 17:01:51 +0530

    Fixes

--- output.tf
+++ output.tf
@@ -1,3 +1,7 @@
 output "private_ip" {
   value = aws_instance.elasticsearch.private_ip
 }
+output "elasticsearch_pem_file" {
+  value = tls_private_key.ssh_private_key.private_key_pem
+  sensitive = true
+}
