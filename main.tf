23f43f3e4e44cd5dd25c4b673ff66f6f12f7ef73
Ketan Joshi <ketanjoshi@TTNs-MacBook-Pro.local>
2023-03-07 09:59:52 +0530

    Updated Security Group

--- main.tf
+++ main.tf
@@ -44,14 +44,14 @@ resource "aws_security_group" "elasticsearch_sg" {
   }
   ingress {
     description = "ingress rules"
-    cidr_blocks = [var.vpc_cidr_block]
+    cidr_blocks = [var.vpc_cidr_block,"182.71.160.184/29","61.12.91.216/29"]
     from_port = 9300
     protocol = "tcp"
     to_port = 9300
   }
   ingress {
     description = "ingress rules"
-    cidr_blocks = [var.vpc_cidr_block]
+    cidr_blocks = [var.vpc_cidr_block,"182.71.160.184/29","61.12.91.216/29"]
     from_port = 9200
     protocol = "tcp"
     to_port = 9200
